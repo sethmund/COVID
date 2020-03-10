a8180de64d6d856571b2a1cf4487e79c6773f356
Seth Edmunds <sethmund@gmail.com>
2020-03-10 11:20:26 -0400

    drop tibble, add xlsx save

--- COVID.R
+++ COVID.R
@@ -1,17 +1,18 @@
-library(tidyverse) 
+library(dplyr)
+library(openxlsx)
 
 date <- "03-09-2020"
 
 case_dat <-  read.csv((paste0("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/", date,".csv"))) %>% 
   select(-Province.State,-Latitude ,-Longitude,-Last.Update) %>% 
   group_by(Country.Region) %>% 
-  summarise_all(tibble::lst(sum)) %>% 
+  summarise_all(list(~sum())) %>% 
   transmute(country=Country.Region,
-            cases = Confirmed_sum,
-            deaths = Deaths_sum)
+            cases = Confirmed,
+            deaths = Deaths)
 
 
-final_dat <- left_join(case_dat, read.csv("data/GHSI.csv")) %>% 
+case_dat <- left_join(case_dat, read.csv("data/GHSI.csv")) %>% 
   left_join(read.csv("data/IRC.csv",na.strings="n/a")) %>% 
   select(region,
          country,
@@ -20,3 +21,5 @@ final_dat <- left_join(case_dat, read.csv("data/GHSI.csv")) %>%
          cases,
          deaths) %>% 
   arrange(desc(cases))
+
+write.xlsx(case_dat,"data/Final.xlsx")
